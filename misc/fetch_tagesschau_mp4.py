5c79dad7d23653a196f7d8f6f5d15e3e5c581a24
Dressler Armon <armon.dressler@swisscom.com>
2022-11-09 17:24:01 +0100

    -

--- misc/fetch_tagesschau_mp4.py
+++ misc/fetch_tagesschau_mp4.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python3
 
 __author__ = "armon.dressler@gmail.com"
-__description__ = """Given a timerange and a destination dir, fetch every tagesschau in mp4 format in the timerange and store it in the destination dir. Inspired by https://github.com/jmesserli/tagesschau-helper -> Some of the tagesschau sessions don't include a direct link to the underlying mp4. There is a feed at https://www.srf.ch/feed/podcast/hd/ff969c14-c5a7-44ab-ab72-14d4c9e427a9.xml which includes said links however. Each session is ~1.1 GBs in size."""
+__description__ = """Given a URL and a destination dir, fetch the tagesschau in mp4 format and store it in the destination dir. Inspired by https://github.com/jmesserli/tagesschau-helper -> Some of the tagesschau sessions don't include a direct link to the underlying mp4. Each session is ~1.1 GBs in size."""
 
 import argparse
+import youtube_dl
+import ffmpeg
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-s", "--start-date",
-                    action="store",
-                    help='ISO 8601 formatted start date to fetch news sessions from. e.g. 2022-09-15'
-                    )
-parser.add_argument('-v', '--verbose',
-                    action='store_true',
-                    help='Verbose output'
-                    )
+parser.add_argument("-u",
+                    "--url",
+                    type=str,
+                    required=True,
+                    help="URL to tagesschau show, e.g. https://www.srf.ch/play/tv/tagesschau/video/tagesschau-vom-08-11-2022-mittagsausgabe?urn=urn:srf:video:caf4a877-e935-48f8-84f5-b42800c06fa7")
+parser.add_argument("-d",
+                    "--dest",
+                    type=str,
+                    default="./tagesschau",
+                    help="Destination dir for video files")
+parser.add_argument("-v", 
+                    "--verbose",
+                    action="store_true",
+                    help="Verbose output")
 args = parser.parse_args()
 
+
+with youtube_dl.YoutubeDL({}) as ydl:
+    ydl.download([args.url])
